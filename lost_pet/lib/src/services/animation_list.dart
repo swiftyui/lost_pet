cd181fe9b1b1463fe08f0959e5227e8959a4a610
SwiftyUI <vanzylarno@yahoo.com>
2023-05-03 08:13:43 +0200

    changes

--- lost_pet/lib/src/services/animation_list.dart
+++ lost_pet/lib/src/services/animation_list.dart
@@ -1,3 +1,4 @@
 class AnimationList {
   static String get dinosaur => 'assets/animations/dinosaur.json';
+  static String get map => 'assets/animations/map.json';
 }
--- lost_pet/lib/src/views/home_screens/home_screen.dart
+++ lost_pet/lib/src/views/home_screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lost_pet/src/models/user_model.dart';
+import 'package:lost_pet/src/services/animation_list.dart';
 import 'package:lost_pet/src/services/authentication_service.dart';
+import 'package:lost_pet/src/services/theme.dart';
 import 'package:lost_pet/src/views/home_screens/home_drawer.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,10 +14,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Lost Pets'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _scaffoldKey.currentState?.openEndDrawer();
+            },
+            icon: const Icon(
+              Icons.notifications_active_outlined,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              _scaffoldKey.currentState?.openEndDrawer();
+            },
+            icon: const Icon(
+              Icons.person_outline,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       body: _buildBody(context),
       endDrawer: const HomeDrawer(),
     );
@@ -28,6 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             children: [
               _welcomeWidget(context, value),
+              _mapWidget(context),
             ],
           ),
         );
@@ -35,9 +64,72 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Widget _mapWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: lightCardBoxShadow,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Pets near me',
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Lottie.asset(AnimationList.map),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _welcomeWidget(BuildContext context, UserModel? userModel) {
-    return Container(
-      child: Text('Hi ${userModel?.displayName}'),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: lightCardBoxShadow,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Hi ${userModel?.displayName}!',
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              RichText(
+                text: TextSpan(
+                  text: "Welcome back to the My Pet app, we've missed you!",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
